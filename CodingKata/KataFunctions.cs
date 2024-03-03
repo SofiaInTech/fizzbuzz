8e14089ceeb1c886ed12e5256a57ac6ec645f71b
Sofia <s.ahmed9833@gmail.com>
2024-03-03 22:35:33 +0000

    Implemented FizzBuzz function for displaying results to console

--- CodingKata/KataFunctions.cs
+++ CodingKata/KataFunctions.cs
@@ -1,15 +1,23 @@
-﻿using System;
+﻿using CodingKata.Service;
+using CodingKata.Service.Interfaces;
+
 namespace CodingKata
 {
 	public static class KataFunctions
 	{
 		public static void FizzBuzz()
 		{
+			// Calling this via some kind of DI library would be ideal.
+			IKataService kataService = new KataService();
+
+			// Compile list of numbers
 			var numbers = Enumerable.Range(1, 100);
 
+			// Iterate over each number and get FizzBuzz result
 			foreach (var number in numbers)
 			{
-				Console.WriteLine(number);
+				var fizzBuzzResult = kataService.CalculateFizzBuzzResult(number);
+				Console.WriteLine(fizzBuzzResult);
 			}
 		}
 	}
